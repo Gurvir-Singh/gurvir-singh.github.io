2b5391c2d963232389c038f4431cf2f84a05b469
Gurvir Singh <charizardzx9@gmail.com>
2026-02-08 21:50:34 -0500

    added changing of content with buttons

--- Main.js
+++ Main.js
@@ -14,10 +14,43 @@ var expContent = document.getElementById("MyExperience");
 var certContent = document.getElementById("MyCertifications");
 var resumeConent = document.getElementById("Resume");
 
+
 skillsContent.style.display = "block";
 expContent.style.display = "none";
 certContent.style.display = "none";
 resumeConent.style.display = "none";
 
+function dispSkills() {
+    skillsContent.style.display = "block";
+    expContent.style.display = "none";
+    certContent.style.display = "none";
+    resumeConent.style.display = "none";
+}
+
+function dispExp() {
+    skillsContent.style.display = "none";
+    expContent.style.display = "block";
+    certContent.style.display = "none";
+    resumeConent.style.display = "none";
+}
+
+function dispCert() {
+    skillsContent.style.display = "none";
+    expContent.style.display = "none";
+    certContent.style.display = "block";
+    resumeConent.style.display = "none";
+}
+
+function dispResume() {
+    skillsContent.style.display = "none";
+    expContent.style.display = "none";
+    certContent.style.display = "none";
+    resumeConent.style.display = "block";
+}
+
+skillsButton.onclick = dispSkills;
+expButton.onclick = dispExp;
+certButton.onclick = dispCert;
+resumeButton.onclick = dispResume;
 
 //dynamicConentDiv.appendChild();
